1418eb6c225d79723fcfc436bfc50d588a992230
Кирилл <vokadk@gmail.com>
2024-12-15 22:23:19 +0300

    Добавьте файлы проекта.

---
+++ Belorukov2024PP/Belorukov2024PP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Belorukov2024PP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
