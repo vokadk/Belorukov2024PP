a01ef10392e243e81436bb9a381f9399261a622c
Кирилл <vokadk@gmail.com>
2024-12-19 22:10:32 +0300

    Day5

--- Belorukov2024PP/Belorukov2024PP/Registracia.xaml.cs
+++ Belorukov2024PP/Belorukov2024PP/Registracia.xaml.cs
@@ -14,7 +14,35 @@ namespace Belorukov2024PP
     {
         public Registracia()
         {
+            NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+        }
+        private async void Reg_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Name.Text))
+            {
+                await DisplayAlert("Ошибка", "Имя не может быть пустым", "OK");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(Login.Text))
+            {
+                await DisplayAlert("Ошибка", "Логин не может быть пустым", "OK");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(Password.Text))
+            {
+                await DisplayAlert("Ошибка", "Пароль не может быть пустым", "OK");
+                return;
+            }
+
+            await DisplayAlert("Успех", "Вы успешно зарегистрировались", "OK");
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
         }
     }
 }
